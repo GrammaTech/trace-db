55fadbeb9344d49cd62a8a5e6e780f711a6ace2c
Nathan Weston <nweston@grammatech.com>
2017-10-17 17:41:55 -0400

    Add helper functions for writing variables.
    WRITE_TRACE_VARIABLE is a fairly large macro which can be invoked
    thousands of times in an instrumented program, leading to long compile
    times. Moving most of it into a function cuts down on the bloat and
    speeds things up.
    
    We still need the macro to create a temporary variable (for writing
    register variables).
    
    write_trace_blob doesn't need to deal with register variables so it
    can be implemented entirely as a function.

--- write-trace.h
+++ write-trace.h
@@ -12,32 +12,19 @@
 # define AUTO __auto_type
 #endif
 
+/* Assigning to tmp here solves two problems:
+   1. If var has the "register" storage class, we can't take its address,
+   but tmp will discard the storage class.
+   2. If var is a static array, sizeof() will give the size of its
+   contents when we want to write its address. But tmp will be
+   a pointer.
+*/
 #define WRITE_TRACE_VARIABLE(out, name_index, type_index, var) \
     do {                                                       \
-        uint16_t val;                                          \
-        fputc(VARIABLE, out);                                  \
-        val = name_index; fwrite(&val, sizeof(val), 1, out);   \
-        val = type_index; fwrite(&val, sizeof(val), 1, out);   \
-        /* Assigning to tmp here solves two problems:
-           1. If var has the "register" storage class, we can't take its address,
-              but tmp will discard the storage class.
-           2. If var is a static array, sizeof() will give the size of its
-              contents when we want to write its address. But tmp will be
-              a pointer.
-        */                                                     \
         AUTO tmp = var;                                        \
-        fwrite(&tmp, sizeof(tmp), 1, out);                     \
+        write_trace_variable(out, name_index, type_index,      \
+                             sizeof(tmp), &tmp);               \
     } while(0)
-
-#define WRITE_TRACE_BLOB(out, name_index, type_index, size, ptr)        \
-    do {                                                                \
-        uint16_t val;                                                   \
-        fputc(VARIABLE, out);                                           \
-        val = name_index; fwrite(&val, sizeof(val), 1, out);            \
-        val = type_index; fwrite(&val, sizeof(val), 1, out);            \
-        val = size; fwrite(&val, sizeof(val), 1, out);                  \
-        fwrite(ptr, size, 1, out);                                      \
-    } while (0)
 
 void write_trace_header(FILE *out, const char **names, uint16_t n_names,
                         const type_description *types, uint16_t n_types);
@@ -45,5 +32,9 @@ void write_trace_id(FILE *out, uint32_t statement_id);
 void write_trace_aux(FILE *out, uint64_t value);
 void write_end_entry(FILE *out);
 void write_buffer_size(FILE *out, void *address, size_t size);
+void write_trace_variable(FILE *out, uint16_t name_index, uint16_t type_index,
+                          uint16_t size, void *var);
+void write_trace_blob(FILE *out, uint16_t name_index, uint16_t type_index,
+                      uint16_t size, void *var);
 
 #endif // __WRITE_TRACE_H
