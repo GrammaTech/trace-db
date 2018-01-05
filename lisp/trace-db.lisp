1290ad49a7cbc78eb4ea25309f8a466b76996f4e
Nathan Weston <nweston@grammatech.com>
2018-01-05 11:27:22 -0500

    Add filter argument to Lisp query interface.
    This allows pruning of results with a Lisp function.

--- lisp/trace-db.lisp
+++ lisp/trace-db.lisp
@@ -408,7 +408,7 @@
        (null-pointer))))
 
 (defmethod query-trace ((db trace-db) index var-names var-types
-                        &key predicate)
+                        &key predicate filter)
   (let* ((n-vars (length var-types))
          (trace-types (trace-types db index))
          ;; Convert type names to indices
@@ -453,10 +453,15 @@
                            results-ptr n-results-ptr)
             (when (and types names)
               (iter (for i below (mem-ref n-results-ptr :uint64))
-                    (collect
-                        (convert-trace-point names types
-                                             (mem-ref results-ptr :pointer)
-                                             i)))))
+                    (let ((point
+                           (convert-trace-point names types
+                                                (mem-ref results-ptr :pointer)
+                                                i)))
+                      (when (or (not filter)
+                                (apply filter
+                                       (cdr (assoc :c point))
+                                       (cdr (assoc :scopes point))))
+                        (collect point))))))
 
          (progn
            ;; Free memory
@@ -470,7 +475,7 @@
            (foreign-free free-vars)))))))
 
 (defmethod query-point ((db trace-db) trace-index point-index
-                        var-names var-types &key predicate)
+                        var-names var-types &key predicate filter)
   (let* ((n-vars (length var-types))
          (trace-types (trace-types db trace-index))
          ;; Convert type names to indices
@@ -510,15 +515,21 @@
                                                i))
                                 (while str)
                                 (collect str result-type 'vector)))))
+            (assert (< point-index (getf trace 'n-points)))
             (c-query-point (db-pointer db) trace-index point-index
                            n-vars free-vars predicate-ptr
                            results-ptr n-results-ptr)
             (when (and types names)
               (iter (for i below (mem-ref n-results-ptr :uint64))
-                    (collect
-                        (convert-trace-point names types
-                                             (mem-ref results-ptr :pointer)
-                                             i)))))
+                    (let ((point
+                           (convert-trace-point names types
+                                                (mem-ref results-ptr :pointer)
+                                                i)))
+                      (when (or (not filter)
+                                (apply filter
+                                       (cdr (assoc :c point))
+                                       (cdr (assoc :scopes point))))
+                        (collect point))))))
 
          (progn
            ;; Free memory
