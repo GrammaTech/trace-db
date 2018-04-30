050ae7bfed79b4bb73c889208b7bae2b00532269
Nathan Weston <nweston@grammatech.com>
2018-04-30 07:54:10 -0400

    Fix writing of 64-bit variables to trace.

--- write-trace.c
+++ write-trace.c
@@ -95,7 +95,7 @@ void write_trace_variables(FILE *out, uint32_t n_vars, ...)
               }
           case 8:
               {
-                  int64_t val = va_arg(ap, int);
+                  int64_t val = va_arg(ap, int64_t);
                   fwrite(&val, sizeof(val), 1, out);
                   break;
               }
